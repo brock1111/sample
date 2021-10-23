15042bd0e69f745a63b4b7735974be1e7efb40a5
brock1111 <82257769+brock1111@users.noreply.github.com>
2021-10-23 10:47:38 +0530

    Update mega.sh

--- mega.sh
+++ mega.sh
@@ -1,19 +1,16 @@
-log_msg() {
-	echo ooof
-}
+
 creds=${1:-"creds.csv"}
 mega-logout 1>/dev/null
 
 IFS=","
 while read username password; do
         echo -e "\n>>> $username"
-        log_msg "Trying to login as $username"
+        
 
         mega-login $username $password >&5
 
         if [ ! $? -eq 0 ]; then
                 echo "Unable to login as $username"
-                log_msg "[ERROR] Unable to login as $username"
                 continue
         fi
 
