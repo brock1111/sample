fb1a39bb849af875f58c4fbf10e2c3c65b3b710f
brock1111 <82257769+brock1111@users.noreply.github.com>
2021-10-23 15:59:39 +0530

    Create mega2.sh

---
+++ mega2.sh
@@ -0,0 +1,13 @@
+creds=${1:-"creds.txt"}
+mega-logout 1>/dev/null
+IFS=","
+while read email pass;do
+        echo -e "\n>>> $email"
+        mega-login $email $pass
+        if [ ! $? -eq 0 ]; then
+                echo "Login failed with $email, So check creds or account expired"
+                continue
+        fi
+        mega-df -h
+        mega-logout 1>/dev/null
+done <$creds
